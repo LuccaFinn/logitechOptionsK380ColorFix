88cbc17c6ccace325c45ed9db6e1e32366236ff4
Lucca Finn <78115169+LuccaFinn@users.noreply.github.com>
2023-09-16 22:30:51 +0200

    Add files via upload

---
+++ k380_blue.bat
@@ -0,0 +1,35 @@
+@echo off
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+::ENTER YOUR CODE BELOW:
+
+
+cd C:\ProgramData\LogiOptionsPlus\depots\200044\k380
+
+ren k380_front.png  k380_front_ext7.png 
+
+ren k380_front_ext2.png k380_front.png 
+
